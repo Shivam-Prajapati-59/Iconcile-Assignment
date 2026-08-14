f0da1a138ad4d27500aae1b23b8b8e1ee5d28e92
Shivam Prajapati <sp488830@gmail.com>
2026-08-14 20:00:04 +0530

    feat: initialize frontend and backend with React, Vite, and Tailwind CSS

---
+++ backend/src/main/java/com/shivam/expensemanager/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.shivam.expensemanager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/api/health")
+    public String health() {
+        return "Expense Manager API is running";
+    }
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  (
+    <div>
+      <h1>
+        Hello World
+      </h1>
+    </div>
+  )
+
+}
+
+export default App
